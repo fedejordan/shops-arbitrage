e00eddfd6e417b32806ee8a949dcadad08313cdd
Federico Jordan <fedejordan99@gmail.com>
2025-04-04 02:49:12 -0300

    added prints and fix field name to update products with own categories script

--- backend/update-products-with-categories.py
+++ backend/update-products-with-categories.py
@@ -7,12 +7,16 @@ def update_products_category_ids(db: Session):
 
     # Tomamos todas las retailer_categories que ya tienen un category_id
     mappings = db.query(RetailerCategory).filter(RetailerCategory.category_id != None).all()
+    mappings_count = len(mappings)
+    current_mapping = 0
 
     for rc in mappings:
+        print(f"🟢 Procesando retailer_category: {rc.name} - category_id: {rc.category_id}")
+        print(f"Actualizando productos para retailer_category {current_mapping + 1} de {mappings_count}")
         # Para cada mapeo aplicamos la categoría al producto correspondiente
         products_to_update = db.query(Product).filter(
             Product.retailer_id == rc.retailer_id,
-            Product.category == rc.name
+            Product.retail_category == rc.name
         ).all()
 
         for product in products_to_update:
@@ -20,6 +24,7 @@ def update_products_category_ids(db: Session):
             updated_count += 1
 
         db.commit()
+        current_mapping += 1
 
     print(f"✅ Se actualizaron {updated_count} productos con su category_id correspondiente.")
 
